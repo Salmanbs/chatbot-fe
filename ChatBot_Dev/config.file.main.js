fbf304a8d866fbce508d0339b557ae74784368fb
Venugopal K R <developer@tosall.com>
2021-03-09 20:29:46 +0530

    docs: added dev folder

---
+++ ChatBot_Dev/config.file.main.js
@@ -0,0 +1,8 @@
+const chatHeaderData = () => {
+    return WebChat.default.init({
+        customData: { "bot_id": "eEw0U2F5emZ2ZkdHZjJ1RkFxd004Zz09" }, // api data
+        baseUrl: "https://workchallenger.com/tosall_client2"
+    })
+}
+
+chatHeaderData();
